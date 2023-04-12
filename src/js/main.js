8d76737fe88a49b607ad976732394fed916466d0
olegrewko <olegrewko@gmail.com>
2023-04-12 19:18:59 +0300

    q

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,47 @@
 import './_vendor';
-import vars from '../js/_vars';
+
 import './_functions';
+import './components/card-bottom-tabs';
 import './components/banner-slider.js';
 import './components/catalog-slider.js';
 import './components/catalog-props.js';
 import './components/color-select.js';
+import './components/card-slider.js';
+import './components/product.js';
 import './components/stepper.js';
 import './components/catalog-filter-toggle.js';
-
+// import 'simplebar/dist/simplebar.css';
 // vars.$freeDeliveryBtn.addEventListener('click', (e) => {
 //   e.currentTarget.closest('.free-delivery').style.display = 'none';
 // });
 
+import vars from '../js/_vars';
+import {resizeContent} from './functions/resize';
+import {scrollTo} from './functions/smooth-scroll';
+import {disableScroll, enableScroll} from './functions/stop-scroll';
+import SimpleBar from 'simplebar';
 
+//disableScroll(fix) // fix -> class of element with position: fixed
+// new SimpleBar(document.getElementById('myElement'));
+if (document.querySelector('[data-bar]')) {
+  new SimpleBar(document.querySelector('.card-description__navigation'));
+}
+
+if (document.querySelector('.to-top')) {
+  document.querySelector('.to-top').addEventListener('click', (e) => {
+    e.preventDefault();
+    scrollTo(document.querySelector('.site-container'));
+  });
+}
+ $(document).ready(function () {
+        var defaults = {
+          containerID: "toTop",
+          containerHoverID: "toTopHover",
+          scrollSpeed: 1200,
+          easingType: "linear",
+        };
+
+        $().UItoTop({
+          easingType: "easeOutQuart",
+        });
+      });
